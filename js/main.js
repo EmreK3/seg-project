53af414e528dd3c78bc5324f48448aca3e599346
Harris Mirza <mirza.harris@gmail.com>
2017-10-19 17:54:58 +0100

    changed JS stuff and some html

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
 let modules = [
 	new Module("6CCS3PRJ", "Individual Project", "6CCS3PRJ", 30, false, false, false, false),
 	new Module("6CCS3AMS", "Agents and Multi-Agent Systems", "6CCS3AMS", 15, false, false, false, true),
-	new Module("6CCS3CSL", "Computer Science Logic", "6CCS3CSL", 15, false, false, false, true),
-	new Module("6CCS3CIS", "Cryptography and Information Security", "6CCS3CIS", 15, false, false, false, true)
+	new Module("6CCS3CFL", "Compiler and Formal Languages", "6CCS3CFL", 15, true, false, true, false),
+	new Module("6CCS3COV", "Computer Vision", "6CCS3COV", 15, false, false, false, true),
+	new Module("6CCS3CSL", "Computer Science Logic", "6CCS3CSL", 15, false, false, false, false),
+	new Module("6CCS3HCI", "Human-Computer Interaction", "6CCS3HCI", 15, false, false, true, false),
+	new Module("6CCS3VER", "Formal Verification", "6CCS3VER", 15, false, false, true, false),
+	new Module("6CCS3WSN", "Algorithms for the World Wide Web and Social Networks", "6CCS3WSN", 15, false, false, true, false),
+	new Module("6CCS3SMT", "Software Measurement and Testing", "6CCS3SMT", 15, false, false, true, false),
+	new Module("6CCS3GRS", "Computer Graphics Systems", "6CCS3GRS", 15, false, true, false, false),
+	new Module("6CCS3PAL", "Parallel Algorithms", "6CCS3PAL", 15, true, true, true, false),
+	new Module("6CCS3AIN", "Artificial Intelligence", "6CCS3AIN", 15, false, false, false, true),
+	new Module("6CCS3CIS", "Cryptography and Information Security", "6CCS3CIS", 15, false, false, false, true),
+	new Module("6CCS3AIP", "Artificial Intelligence Planning", "6CCS3AIP", 15, false, false, false, true),
 ];
+
+let management = false;
 
 function printSelectedModules()
 {
@@ -16,12 +28,41 @@ function printSelectedModules()
 	});
 }
 
+function calculateTotalCredits()
+{
+	let total = 0;
+	modules.forEach(function(module)
+	{
+		if(module.selected == true)
+		{
+			total += module.creditValue; 
+		}
+	});
+
+	return total;
+}
+
 $(document).ready(function()
 {
 	console.log("Ready!");
 	modules.forEach(function(module)
 	{
-		//Get HTML Element and add onClick listener
+
+		$("#" + module.elementId + "_description").click(function()
+		{
+			console.log("Opened " + module.moduleCode + " Description");
+			//If clicked toggle description and button
+			if($("#" + module.elementId + "_description").text() == "+")
+			{
+				$("#" + module.elementId + "_description").text("-");
+				$("#" + module.elementId + "_description").removeClass("hidden");
+			}else
+			{
+				$("#" + module.elementId + "_description").text("+");
+				$("#" + module.elementId + "_description").addClass("hidden");
+			}
+		});
+
 		$("#" + module.elementId).click(function()
 		{
 			console.log("Toggled " + module.moduleCode);
@@ -30,6 +71,8 @@ $(document).ready(function()
 			module.selected = !module.selected;
 			printSelectedModules();
 		});
+
+		
 
 	});
 })
